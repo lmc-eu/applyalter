bd33aedb0bb93a57f19b73a0fea0dd9ce4bba171
mcaslavsky <mcaslavsky@bb798713-c5b0-4250-8689-17baff6db330>
2008-05-26 12:26:28 +0000

    add build and run scripts
    git-svn-id: https://svn.int.lmc.cz/svn/G2/trunk@36156 bb798713-c5b0-4250-8689-17baff6db330

---
+++ applyalter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+xDEBUG="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5000"
+
+# Platform dependent CLASSPATH separator
+if [ "Windows_NT" = "$OS" ]
+then
+  CPS=\;
+else
+  CPS=:
+fi
+
+for i in `find . -name '*.jar'`; do 
+  CP="$CP$CPS$i"
+done
+
+java $DEBUG -cp $CP ch.ips.g2.applyalter.ApplyAlter $*
