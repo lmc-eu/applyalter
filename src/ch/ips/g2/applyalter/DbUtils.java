cb58f069873f7c1fbff6ed777d67817c96aadf7a
ptisnovsky <ptisnovsky@bb798713-c5b0-4250-8689-17baff6db330>
2008-11-14 15:36:37 +0000

    Fixed typos
    git-svn-id: https://svn.int.lmc.cz/svn/G2/trunk@40665 bb798713-c5b0-4250-8689-17baff6db330

--- src/ch/ips/g2/applyalter/DbUtils.java
+++ src/ch/ips/g2/applyalter/DbUtils.java
@@ -19,7 +19,7 @@ public class DbUtils
    * Close statement, do nothing if it's not open.
    * No exception is thrown, ever; this method is thus safe to use inside finally clause.
    *
-   * @param stmt statemenet to close, null is handled (nothing is done in such case)
+   * @param stmt statement to close, null is handled (nothing is done in such case)
    */
   public static void close( Statement stmt )
   {
@@ -36,10 +36,10 @@ public class DbUtils
   }
 
   /**
-   * Close result set and statement (any of them or both can be mmissing).
+   * Close result set and statement (any of them or both can be missing).
    * No exception is thrown, ever; this method is thus safe to use inside finally clause.
    *
-   * @param stmt statemenet to close, null is handled (nothing is done in such case)
+   * @param stmt statement to close, null is handled (nothing is done in such case)
    * @param rs result set to close, null is handled (nothing is done in such case)
    */
   public static void close( Statement stmt, ResultSet rs )
