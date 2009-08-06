77f6025b2ff17eddf80b48c6654e52b1247a711a
jbares <jbares@bb798713-c5b0-4250-8689-17baff6db330>
2009-08-06 11:03:04 +0000

    Fix new FindBugs warnings
    git-svn-id: https://svn.int.lmc.cz/svn/G2/trunk@46062 bb798713-c5b0-4250-8689-17baff6db330

--- src/ch/ips/g2/applyalter/ZipEntryNameComparator.java
+++ src/ch/ips/g2/applyalter/ZipEntryNameComparator.java
@@ -1,14 +1,18 @@
 package ch.ips.g2.applyalter;
+
+import java.io.Serializable;
 import java.util.Comparator;
 import java.util.zip.ZipEntry;
 
 /**
- * Alphabeticaly compares {@link ZipEntry ZipEntries} according to their {@link ZipEntry#getName()}
+ * Alphabetically compares {@link ZipEntry ZipEntries} according to their {@link ZipEntry#getName()}
+ * 
  * @author Martin Caslavsky &lt;martin.caslavsky@ips-ag.cz&gt;
  * @version $Id$
  */
+@SuppressWarnings( "serial" )
 public class ZipEntryNameComparator implements
-    Comparator<ZipEntry>
+ Comparator<ZipEntry>, Serializable
 {
   public int compare(ZipEntry i, ZipEntry j) {
     if (i.equals(j))
