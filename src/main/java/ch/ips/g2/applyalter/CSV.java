97661b585e944a0df6f0c7bc52b18f8a4116cb69
Radim Kubacki <radim@kubacki.cz>
2013-03-06 13:15:50 +0100

    Read CVS resources using UTF-8 encoding

--- src/main/java/ch/ips/g2/applyalter/CSV.java
+++ src/main/java/ch/ips/g2/applyalter/CSV.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang.StringUtils;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.sql.*;
 import java.util.Arrays;
 import java.util.Map;
@@ -60,7 +61,7 @@ public class CSV extends AbstractStatement {
             throw new ApplyAlterException(String.format("missing top-level element: <datafile>%s</datafile>", getFile()));
         }
 
-        CSVReader rdr = new CSVReader(new InputStreamReader(new ByteArrayInputStream(rawFile)));
+        CSVReader rdr = new CSVReader(new InputStreamReader(new ByteArrayInputStream(rawFile), Charset.forName("UTF-8")));
 
         PreparedStatement st = null;
         try {
